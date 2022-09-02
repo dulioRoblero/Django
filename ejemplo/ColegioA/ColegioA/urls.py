994b0cfc292b619d83f8bbc8e3a17e3d20a05613
duliodemian <duliodamianperez@gmail.com>
2022-09-02 12:00:27 -0600

    actividad7

--- ejemplo/ColegioA/ColegioA/urls.py
+++ ejemplo/ColegioA/ColegioA/urls.py
@@ -13,9 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+<<<<<<< HEAD
+"""from xml.etree.ElementInclude import include"""
+from django.contrib import admin
+from django.urls import path,include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('home/', include('Apps.home.urls','home'))
+=======
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+>>>>>>> 2d18fdc7fa471218cd26f7a84e5f16eb102b3189
 ]
